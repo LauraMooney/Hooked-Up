3487b5e57718875f217f5ea1e096437de96ce89f
Laura Mooney <79043901+LauraMooney@users.noreply.github.com>
2022-03-14 14:27:59 +0000

    updated to remove react strap

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,16 @@
 import React from "react";
 import './App';
 import './App.css';
-import { Container, Nav, } from "reactstrap";
 
 function Footer() {
   return (
     <footer className="footer">
-    <Container fluid>
-      <Nav>
         <p className='footercontent'>
          Created using Spotify API and React
         </p>
-      </Nav>
       <div className="copyright">
-        © {new Date().getFullYear()} made by Laura Mooney
+        © 2022 made by Laura Mooney
       </div>
-    </Container>
   </footer>
   );
 }
