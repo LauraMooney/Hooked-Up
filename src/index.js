20819f79c2426493718db4650414b475e51f1f5a
Laura Mooney <79043901+LauraMooney@users.noreply.github.com>
2022-03-14 14:32:45 +0000

    updated to deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Footer from './Footer';
 import reportWebVitals from './reportWebVitals';
 
 
